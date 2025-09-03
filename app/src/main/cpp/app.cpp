8fb8557d95b70a7142348d23356ed803022e9134
Michael Volikov <33529847+pentryyy@users.noreply.github.com>
2025-09-03 20:54:41 +0300

    Change project structure (#1)
    * По мелочи
    
    * Перенесено из lib в app
    
    * Проект переделан на app
    
    * Create README.md

--- app/src/main/cpp/app.cpp
+++ app/src/main/cpp/app.cpp
@@ -4,8 +4,14 @@
 
 #include <iostream>
 #include <stdlib.h>
-#include "demo.h"
+#include "app.h"
 
 std::string demo::Greeter::greeting() {
     return std::string("Hello, World!");
+}
+
+int main () {
+    demo::Greeter greeter;
+    std::cout << greeter.greeting() << std::endl;
+    return 0;
 }
