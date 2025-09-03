fbc9a8b0e2b868aebc9f6077ce288eccd3f61b60
pentryyy <m.v.volikov@gmail.com>
2025-09-03 22:47:47 +0300

    Изменено отображение картинки

--- main.py
+++ main.py
@@ -14,7 +14,39 @@ def main():
         print(f"Ошибка: не удалось загрузить изображение из {image_path}")
         exit()
     
-    cv2.imshow('Image', image)
+    height, width = image.shape[:2]
+    
+    center_coordinates = (
+        width // 2, 
+        height // 2 - 140
+    )
+    radius = 100
+    circle_color = (0, 255, 0)
+    thickness = 3
+    
+    cv2.circle(image, center_coordinates, radius, circle_color, thickness)
+    
+    text = "Hello OpenCV!"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    text_color = (160, 160, 160)
+    text_thickness = 2
+    
+    text_size = cv2.getTextSize(text, font, font_scale, text_thickness)[0]
+    text_x = (width - text_size[0]) // 2
+    text_y = height // 2 + radius + 50
+    
+    cv2.putText(
+        image, 
+        text, 
+        (text_x, text_y), 
+        font, 
+        font_scale, 
+        text_color, 
+        text_thickness
+    )
+    
+    cv2.imshow("OpenCV Image Showing", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
